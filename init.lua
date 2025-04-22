3a1a943a996d08268a10295eda36dd8fa69287bc
Callica <125240398+Callica@users.noreply.github.com>
2025-04-22 07:06:43 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -6,20 +6,17 @@ end
 -- Defina seu usuário e repositório
 local user = "Callica"
 local repo = "Calisa-GUI"
-local base = "https://raw.githubusercontent.com/"..user.."/"..repo.."/main/"
 
 -- Load SplashScreen
 pcall(function()
-    load(base.."SplashScreen.lua")
+    load("https://raw.githubusercontent.com/"..user.."/"..repo.."/main/SplashScreen.lua")
 end)
 
 -- Load GUI
-local builder = load(base.."PanelBuilder.lua")
-if builder and builder.Create then
-    builder.Create()
-end
+local gui = load("https://raw.githubusercontent.com/"..user.."/"..repo.."/main/PanelBuilder.lua")
+local built = gui and gui.Create and gui:Create()
 
 -- Load Main
 pcall(function()
-    load(base.."main.lua")
+    load("https://raw.githubusercontent.com/"..user.."/"..repo.."/main/main.lua")
 end)
